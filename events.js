5615dff569c9dcdb27af0bf6d6fc393aea890bd6
marcinmagala <marcinmagala@gmail.com>
2022-10-05 01:44:50 +0200

    events with server

--- events.js
+++ events.js
@@ -1,4 +1,5 @@
 const EventEmitter = require("events");
+const http = require("http");
 
 class Sales extends EventEmitter {
   constructor() {
@@ -23,3 +24,21 @@ myEmitter.on("newSale", (stock) => {
 myEmitter.emit("newSale", 9);
 
 ////////////////////////
+
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  console.log("Request recived!");
+  res.end("Request recived");
+});
+server.on("request", (req, res) => {
+  console.log("Another request");
+});
+
+server.on("close", () => {
+  console.log("Server closed");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Waiting for requests...");
+});

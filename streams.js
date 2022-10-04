2320b86a7d66714610bb45cfa4c0f114af4b7607
marcinmagala <marcinmagala@gmail.com>
2022-10-05 01:58:15 +0200

    solution 1

--- streams.js
+++ streams.js
@@ -2,7 +2,9 @@ const fs = require("fs");
 const server = require("http").createServer();
 
 server.on("request", (req, res) => {
+  // Solution 1
   fs.readFile("test-file.txt", (err, data) => {
+    if (err) console.log(err);
     res.end(data);
   });
 });

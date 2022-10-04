4235e1f5bd8fc5ad8dd7b3c8c79b12f065473b9a
marcinmagala <marcinmagala@gmail.com>
2022-10-05 01:23:38 +0200

    init events

---
+++ events.js
@@ -0,0 +1,25 @@
+const EventEmitter = require("events");
+
+class Sales extends EventEmitter {
+  constructor() {
+    super();
+  }
+}
+
+const myEmitter = new Sales();
+
+myEmitter.on("newSale", () => {
+  console.log("There was a new sale!");
+});
+
+myEmitter.on("newSale", () => {
+  console.log("Customer name: Marcin");
+});
+
+myEmitter.on("newSale", (stock) => {
+  console.log(`There are now ${stock} items left in stock.`);
+});
+
+myEmitter.emit("newSale", 9);
+
+////////////////////////

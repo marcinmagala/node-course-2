c2cb11aaf78fe25abb77f57cb1520d973021c976
marcinmagala <marcinmagala@gmail.com>
2022-10-05 02:13:35 +0200

    solution 2

--- streams.js
+++ streams.js
@@ -3,9 +3,22 @@ const server = require("http").createServer();
 
 server.on("request", (req, res) => {
   // Solution 1
-  fs.readFile("test-file.txt", (err, data) => {
-    if (err) console.log(err);
-    res.end(data);
+  //   fs.readFile("test-file.txt", (err, data) => {
+  //     if (err) console.log(err);
+  //     res.end(data);
+  //   });
+  // Solution 2: Streams
+  const readable = fs.createReadStream("test-file.txt");
+  readable.on("data", (chunck) => {
+    res.write(chunck);
+  });
+  readable.on("end", () => {
+    res.end();
+  });
+  readable.on("error", (err) => {
+    console.log(err);
+    res.statusCode = 500;
+    res.end("File not found!");
   });
 });
 
